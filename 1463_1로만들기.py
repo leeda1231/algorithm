511b72aa3976413841d587f0e4fda2c74e2fbd53
leeda1231 <dlekdls1231@gmail.com>
2022-06-28 14:26:03 +0900

    0628

---
+++ 1463_1로만들기.py
@@ -0,0 +1,13 @@
+n = int(input())
+dp = [0] * (n+1)
+for i in range(2,n+1):
+    if i % 2 and i % 3:
+        dp[i] = dp[i-1]+1
+    elif i % 2 == 0 and i % 3:
+        dp[i] = min(dp[i-1]+1, dp[i//2]+1)
+    elif i % 3 == 0 and i % 2:
+        dp[i] = min(dp[i-1]+1, dp[i//3]+1)
+    elif i % 2 == 0 and i % 3 == 0:
+        dp[i] = min(dp[i-1]+1, dp[i//2]+1, dp[i//3]+1)
+
+print(dp[-1])
