034bb5f9802a778c6ca1579e1740b852e0c9e6dc
leeda1231 <dlekdls1231@gmail.com>
2022-10-22 23:18:14 +0900

    221022

---
+++ 큰수만들기.py
@@ -0,0 +1,24 @@
+# 시간초과
+def solution(number, k):
+    answer = ''
+    num_lst = list(map(int,number))
+    n = len(number) - k
+    start = 0
+    end = k
+    while n > 0:
+        # 정지조건 start = end?
+        if start == end:
+            answer += ''.join(map(str,num_lst[start:]))
+            break
+        max_val = 0
+        new_start = start + 1
+        for i in range(start,end+1):
+            if num_lst[i] > max_val:
+                max_val = num_lst[i]
+                new_start = i+1
+        start = new_start
+        end += 1
+        answer += str(max_val)
+        n -= 1
+                
+    return answer
