8847cf4f5a09962580daa18392c17a5d1ffd5675
leeda1231 <dlekdls1231@gmail.com>
2022-10-27 22:47:46 +0900

    221027

--- 기능개발.py
+++ 기능개발.py
@@ -1,4 +1,26 @@
-def solution(progresses, speeds):
+import math
+from collections import deque
+
+def solution2(progresses, speeds):
+    answer = []
+    work = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
+    print(work)
+    q = deque(work)
+    longDay = q[0]
+    day = 0
+    while q:
+        x = q.popleft()
+        if x > longDay:
+            longDay = x
+            answer.append(day)
+            day = 1
+        else:
+            day += 1
+    answer.append(day)          
+    return answer
+
+
+def solution1(progresses, speeds):
     answer = []
     while progresses:
         cnt = 0
