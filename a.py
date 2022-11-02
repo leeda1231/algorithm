e6edca706a958cd59d9720a4ace5513f0d25454b
leeda1231 <dlekdls1231@gmail.com>
2022-11-02 23:26:56 +0900

    221102

--- a.py
+++ a.py
@@ -1,23 +1,7 @@
 from collections import deque
 
-def solution(bridge_length, weight, truck_weights):
-    n = len(truck_weights)
-    answer = 0
-    q = deque([0]*bridge_length)
-    total = 0
-    for i in range(n):
-        while 1:
-            tmp = q.pop()
-            total -= tmp
-            if total + truck_weights[i] <= weight:
-                total += truck_weights[i]
-                q.appendleft(truck_weights[i])
-                answer += 1
-                break
-            else:
-                q.appendleft(0)
-                answer += 1
-    
-    answer += len(q)
-        
-    return answer
+q = deque((1,2))
+w = deque()
+w.append((1,2))
+print(q)
+print(w)
