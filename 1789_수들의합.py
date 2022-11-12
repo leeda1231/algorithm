475c24ccb6f22f81ce023e30809be5bdb1bec10f
leeda1231 <dlekdls1231@gmail.com>
2022-11-12 20:58:56 +0900

    221112

--- 1789_수들의합.py
+++ 1789_수들의합.py
@@ -8,25 +8,24 @@ def binary_search(start,end):
         mid = (start + end) // 2
         total = mid * (1+mid) // 2
         if total == s:
-            break
+            return mid
         if total > s:
             end = mid - 1
         else:
             start = mid + 1
-    return mid
+    return end
 
 print(binary_search(start,end))
 
-'''
-n = 1
-total = 0
-while 1:
-    total += n
-    if total == s:
-        break
-    elif total > s:
-        n -= 1
-        break
-    n += 1
-print(n)
-'''
+# s = int(input())
+# n = 1
+# total = 0
+# while 1:
+#     total += n
+#     if total == s:
+#         break
+#     elif total > s:
+#         n -= 1
+#         break
+#     n += 1
+# print(n)
--- a.py
+++ a.py
@@ -1,3 +1,11 @@
-n = 4
-n //= 2
-print(1//2)
+
+def check(lst):
+    k = len(lst)
+    print(lst[:k//2])
+    print(lst[k//2:])
+    if sum(lst[:k//2]) == sum(lst[k//2:]):
+        return 1
+    return 0
+
+lst = (1,4,3,1,4)
+print(check(lst))
