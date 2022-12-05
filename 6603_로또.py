5bc4ddf0628ca8c7592c019c0197416aacca3a35
leeda1231 <dlekdls1231@gmail.com>
2022-12-05 17:11:15 +0900

    221205

---
+++ 6603_로또.py
@@ -0,0 +1,30 @@
+# 재귀
+def dfs(start):
+    if len(ans) == 6:
+        print(*ans)
+        return
+    for i in range(start,k):
+        if s[i] not in ans:
+            ans.append(s[i])
+            dfs(i+1)
+            ans.pop()
+
+while 1:
+    k,*s = map(int,input().split())
+    if k == 0:
+        break
+    ans = []
+    dfs(0)
+    print()
+
+
+# combinations
+from itertools import combinations
+
+while 1:
+    k,*s = map(int,input().split())
+    if k == 0:
+        break
+    for c in combinations(s,6):
+        print(*c)
+    print()
