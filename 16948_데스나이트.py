c60c6d48923f909488d8f5e8ff198748f4d68029
leeda1231 <dlekdls1231@gmail.com>
2022-12-17 20:03:50 +0900

    221217

---
+++ 16948_데스나이트.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+sys.setrecursionlimit
+
+n = int(input())
+r1,c1,r2,c2 = map(int,input().split())
+board = [[0]*n for _ in range(n)]
+dx = [-2,-2,0,0,2,2]
+dy = [-1,1,-2,2,-1,1]
+v = [[0]*n for _ in range(n)]
+v[r1][c1] = 1
+q = deque([(r1,c1)])
+board[r2][c2] = -1
+
+while q:
+    x,y = q.popleft()
+    if x == r2 and y == c2:
+        break
+    for d in range(6):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < n and 0 <= ny < n and v[nx][ny] == 0:
+            board[nx][ny] = board[x][y] + 1
+            v[nx][ny] = 1
+            q.append((nx,ny))
+
+print(board[r2][c2])
