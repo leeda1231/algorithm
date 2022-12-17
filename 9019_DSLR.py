f33d2b6586250c3e3e2a291c737f91f041852fb7
leeda1231 <dlekdls1231@gmail.com>
2022-12-18 00:06:49 +0900

    221218

--- 9019_DSLR.py
+++ 9019_DSLR.py
@@ -11,6 +11,56 @@ def S(n):
     return n-1
 
 def L(n):
+    a = n // 1000
+    b = n * 10
+    return b - 9999*a
+
+def R(n):
+    a = n // 10
+    b = n % 10
+    return 1000*b + a
+
+def bfs(x,y):
+    q = deque([[x,""]])
+    v = [0] * 10000
+    v[x] = 1
+    while q:
+        x,command = q.popleft()
+        if x == y:
+            print(command)
+            break
+        if v[D(x)] == 0:
+            v[D(x)] = 1
+            q.append([D(x),command+"D"])
+        if v[S(x)] == 0:
+            v[S(x)] = 1
+            q.append([S(x),command+"S"])
+        if v[L(x)] == 0:
+            v[L(x)] = 1
+            q.append([L(x),command+"L"])
+        if v[R(x)] == 0:
+            v[R(x)] = 1
+            q.append([R(x),command+"R"])
+
+for _ in range(T):
+    a,b = map(int,input().split())
+    bfs(a,b)
+
+'''
+# 이전 풀이
+from collections import deque
+
+T = int(input())
+
+def D(n):
+    return (2*n)%10000
+
+def S(n):
+    if n == 0:
+        return 9999
+    return n-1
+
+def L(n):
     return n*10 - 9999*(n//1000)
 
 def R(n):
@@ -43,40 +93,40 @@ for _ in range(T):
     print(ans)
 
 
+# 메모리 초과
+from collections import deque
 
-# 메모리초과
-# from collections import deque
+T = int(input())
 
-# T = int(input())
+def D(n):
+    return (2*n)%10000
 
-# def D(n):
-#     return (2*n)%10000
+def S(n):
+    if n == 0:
+        return 9999
+    return n-1
 
-# def S(n):
-#     if n == 0:
-#         return 9999
-#     return n-1
+def L(n):
+    return n*10 - 9999*(n//1000)
 
-# def L(n):
-#     return n*10 - 9999*(n//1000)
+def R(n):
+    return n//10 + n%10*1000
 
-# def R(n):
-#     return n//10 + n%10*1000
+def bfs(n,m):
+    q = deque([[n]])
+    while q:
+        x = q.popleft()
+        if x[0] == m:
+            return x[1:]
+        q.append([D(x[0])] + x[1:] + ['D'])
+        q.append([S(x[0])] + x[1:] + ['S'])
+        if x[-1] != 'R':
+            q.append([L(x[0])] + x[1:] + ['L'])
+        if x[-1] != 'L':
+            q.append([R(x[0])] + x[1:] + ['R'])
 
-# def bfs(n,m):
-#     q = deque([[n]])
-#     while q:
-#         x = q.popleft()
-#         if x[0] == m:
-#             return x[1:]
-#         q.append([D(x[0])] + x[1:] + ['D'])
-#         q.append([S(x[0])] + x[1:] + ['S'])
-#         if x[-1] != 'R':
-#             q.append([L(x[0])] + x[1:] + ['L'])
-#         if x[-1] != 'L':
-#             q.append([R(x[0])] + x[1:] + ['R'])
-
-# for _ in range(T):
-#     a,b = map(int,input().split())
-#     ans = bfs(a,b)
-#     print(''.join(ans))
+for _ in range(T):
+    a,b = map(int,input().split())
+    ans = bfs(a,b)
+    print(''.join(ans))
+'''
--- a.py
+++ a.py
@@ -1,12 +1,4 @@
-n,m = map(int,input().split())
-s = []
-
-def dfs(s):
-    if len(s) == m:
-        print(' '.join(map(str,s))) # join은 int형 안됨
-        return
-       
-    for i in range(1,n+1):
-        dfs(s+[i])
-
-dfs([])
+n = 1234
+a = n // 10
+b = n % 10
+print(1000*b + a)
