72bc6bde5c4a19c64f2a7639cf8bacd79b3e016a
leeda1231 <dlekdls1231@gmail.com>
2022-12-22 00:26:14 +0900

    221222

--- 14891_톱니바퀴.py
+++ 14891_톱니바퀴.py
@@ -1,75 +1,33 @@
 from collections import deque
 
-g1 = deque(map(int,input()))
-g2 = deque(map(int,input()))
-g3 = deque(map(int,input()))
-g4 = deque(map(int,input()))
+for i in range(1,5):
+    globals()[f'g{i}'] = deque(map(int,input()))
+
 rotate_lst = [0,0,0,0]
-
-def rotate_r(q):
-    q.appendleft(q.pop())
-    return
-
-def rotate_l(q):
-    q.append(q.popleft())
-    return
 
 def move_r(n):
     if n == 4:
         return
-    if n == 1:
-        if g1[2] == g2[-2]:
-            return
-        if rotate_lst[0] == 1:
-            rotate_lst[1] = -1
-        else:
-            rotate_lst[1] = 1
-        move_r(n+1)
-    elif n == 2:
-        if g2[2] == g3[-2]:
-            return
-        if rotate_lst[1] == 1:
-            rotate_lst[2] = -1
-        else:
-            rotate_lst[2] = 1
-        move_r(n+1)
-    elif n == 3:
-        if g3[2] == g4[-2]:
-            return
-        if rotate_lst[2] == 1:
-            rotate_lst[3] = -1
-        else:
-            rotate_lst[3] = 1
-        move_r(n+1)
+    if globals()[f'g{n}'][2] == globals()[f'g{n+1}'][-2]:
+        return
+    if rotate_lst[n-1] == 1:
+        rotate_lst[n] = -1
+    else:
+        rotate_lst[n] = 1
+    move_r(n+1)
 
 
 def move_l(n):
     if n == 1:
         return
-    if n == 2:
-        if g1[2] == g2[-2]:
-            return
-        if rotate_lst[1] == 1:
-            rotate_lst[0] = -1
-        else:
-            rotate_lst[0] = 1
-        move_l(n-1)
-    elif n == 3:
-        if g2[2] == g3[-2]:
-            return
-        if rotate_lst[2] == 1:
-            rotate_lst[1] = -1
-        else:
-            rotate_lst[1] = 1
-        move_l(n-1)
-    elif n == 4:
-        if g3[2] == g4[-2]:
-            return
-        if rotate_lst[3] == 1:
-            rotate_lst[2] = -1
-        else:
-            rotate_lst[2] = 1
-        move_l(n-1)
+    
+    if globals()[f'g{n-1}'][2] == globals()[f'g{n}'][-2]:
+        return
+    if rotate_lst[n-1] == 1:
+        rotate_lst[n-2] = -1
+    else:
+        rotate_lst[n-2] = 1
+    move_l(n-1)
 
 
 k = int(input())
@@ -79,31 +37,15 @@ for _ in range(k):
     rotate_lst[n-1] = d
     move_r(n)
     move_l(n)
-    if rotate_lst[0] == 1:
-        rotate_r(g1)
-    elif rotate_lst[0] == -1:
-        rotate_l(g1)
-    if rotate_lst[1] == 1:
-        rotate_r(g2)
-    elif rotate_lst[1] == -1:
-        rotate_l(g2)
-    if rotate_lst[2] == 1:
-        rotate_r(g3)
-    elif rotate_lst[2] == -1:
-        rotate_l(g3)
-    if rotate_lst[3] == 1:
-        rotate_r(g4)
-    elif rotate_lst[3] == -1:
-        rotate_l(g4)
+    for i in range(4):
+        if rotate_lst[i] == 1:
+            globals()[f'g{i+1}'].rotate()
+        elif rotate_lst[i] == -1:
+            globals()[f'g{i+1}'].rotate(-1)
 
 ans = 0
-if g1[0] == 1:
-    ans += 1
-if g2[0] == 1:
-    ans += 2
-if g3[0] == 1:
-    ans += 4
-if g4[0] == 1:
-    ans += 8
+for i in range(1,5):
+    if globals()[f'g{i}'][0] == 1:
+        ans += 2**(i-1)
 
 print(ans)
--- a.py
+++ a.py
@@ -1,4 +1,11 @@
 from collections import deque
-q = deque(map(int,input()))
-q.appendleft(1)
+q = deque([1,2,3])
+
+q.rotate(-1)
 print(q)
+
+
+lst = [1,2,3,4,5,6]
+locals()['test1'] = [1,2,3,4,5]
+
+print(test1)
