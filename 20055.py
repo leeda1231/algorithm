7b02b2a78676a553179185a53890a5f87b4d9865
leeda1231 <dlekdls1231@gmail.com>
2023-01-04 02:09:49 +0900

    230104

--- 20055.py
+++
@@ -1,2 +0,0 @@
-n,k = map(int,input().split())
-belt = list(map(int,input().split()))
---
+++ 20055_컨베이어벨트위의로봇.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n,k = map(int,input().split())
+belt = deque(map(int,input().split()))
+robot = deque([0]*n)
+ans = 0
+while 1:
+    ans += 1
+    # 1
+    belt.rotate()
+    robot.rotate()
+    robot[n-1] = 0
+    # 2
+    for i in range(n-2,-1,-1):
+        if robot[i] == 1 and robot[i+1] == 0 and belt[i+1] > 0:
+            if i != n-2:
+                robot[i+1] = 1
+            robot[i] = 0
+            belt[i+1] -= 1
+    # 3
+    if belt[0] != 0:
+        robot[0] = 1
+        belt[0] -= 1
+    # 4
+    if belt.count(0) >= k:
+        print(ans)
+        break
