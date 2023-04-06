a57a7d1308d50380fbac5693e573b66a8e749720
leeda1231 <97644147+leeda1231@users.noreply.github.com>
2023-04-06 14:48:55 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def solution(today, terms, privacies):
+    answer = []
+    ty,tm,td = today.split('.')
+    ty,tm,td = int(ty), int(tm), int(td)
+    term = defaultdict(int)
+    for t in terms:
+        x,y = t.split()
+        term[x] = int(y)
+    for i in range(len(privacies)):
+        date,s = privacies[i].split()
+        y,m,d = date.split('.')
+        y,m,d = int(y),int(m),int(d)
+        nm = m + term[s]
+        nd = d - 1
+        if nd == 0:
+            nd = 28
+            nm -= 1
+        if nm%12 == 0:
+            ny = y + nm//12 -1
+        else:
+            ny = y + nm//12
+        nm %= 12
+        if nm == 0:
+            nm = 12
+        if ny < ty:
+            answer.append(i+1)
+        elif ny == ty and nm < tm:
+            answer.append(i+1)
+        elif ny == ty and nm == tm and nd < td:
+            answer.append(i+1)
+    return answer
